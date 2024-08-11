dfefdec8d6abfe3c8c4e64f9d1ce385487d6f118
Femoon <yjh638725@gmail.com>
2024-08-11 16:27:20 +0800

    feat: add mobile icon

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -19,6 +19,15 @@ export default function RootLayout({ children, params }: { children: React.React
   const lang = params.lang === 'cn' ? 'zh-CN' : 'en'
   return (
     <html lang={lang} data-overlayscrollbars-initialize>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body data-overlayscrollbars-initialize>
         <Analytics />
         <SpeedInsights />
