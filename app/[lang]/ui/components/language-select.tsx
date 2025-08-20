e7fc997492f0c90bb6310bba2b620580030430c9
Femoon <yjh638725@gmail.com>
2025-08-20 17:13:35 +0800

    feat: enhance language selection component to prioritize specific languages

--- app/[lang]/ui/components/language-select.tsx
+++ app/[lang]/ui/components/language-select.tsx
@@ -1,9 +1,34 @@
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 import { Autocomplete, AutocompleteItem } from '@heroui/autocomplete'
 
-import { LanguageSelectProps } from '../../../lib/types'
+import { LangsItem, LanguageSelectProps } from '../../../lib/types'
 
 const LanguageSelect = ({ t, langs, selectedLang, handleSelectLang }: LanguageSelectProps) => {
+  const sortedLangs = useMemo(() => {
+    if (!langs) return { priorityLangs: [], otherLangs: [] }
+
+    // priority langs
+    const priorityLanguages = ['zh-CN', 'en-US', 'ja-JP', 'ko-KR', 'es-US', 'fr-FR', 'de-DE']
+    const priorityLangs: LangsItem[] = []
+    const otherLangs: LangsItem[] = []
+
+    // separate priority langs and other langs
+    langs.forEach(lang => {
+      if (priorityLanguages.includes(lang.value)) {
+        priorityLangs.push(lang)
+      } else {
+        otherLangs.push(lang)
+      }
+    })
+
+    // sort priority langs
+    priorityLangs.sort((a, b) => {
+      return priorityLanguages.indexOf(a.value) - priorityLanguages.indexOf(b.value)
+    })
+
+    return { priorityLangs, otherLangs }
+  }, [langs])
+
   return (
     <Autocomplete
       label={t['select-language']}
@@ -11,9 +36,20 @@ const LanguageSelect = ({ t, langs, selectedLang, handleSelectLang }: LanguageSe
       className="w-full"
       onSelectionChange={handleSelectLang}
     >
-      {langs!.map(item => (
-        <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
-      ))}
+      {[
+        // priority langs
+        ...sortedLangs.priorityLangs.map(item => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>),
+        // separator
+        ...(sortedLangs.priorityLangs.length > 0 && sortedLangs.otherLangs.length > 0
+          ? [
+              <AutocompleteItem key="separator" isDisabled className="!cursor-default">
+                <div className="w-full h-px bg-gray-300 dark:bg-gray-600 my-1"></div>
+              </AutocompleteItem>,
+            ]
+          : []),
+        // other langs
+        ...sortedLangs.otherLangs.map(item => <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>),
+      ]}
     </Autocomplete>
   )
 }
